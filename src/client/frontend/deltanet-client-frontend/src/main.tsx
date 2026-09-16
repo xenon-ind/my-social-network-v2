cf47fb08d38d86dae5391d6055f785446403fe59
SeniorShadifer <discordeg@mail.ru>
2026-09-16 16:44:43 +0300

    feat(client/frontend): init mantine project

--- src/client/frontend/deltanet-client-frontend/src/main.tsx
+++ src/client/frontend/deltanet-client-frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
